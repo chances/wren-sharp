ae77be02b55c875cd8978d5eac994b220b5735dd
Chance Snow <git@chancesnow.me>
2019-12-01 17:32:30 -0800

    Stub out a bunch of wren functions with TODOs as needed

--- WrenSharp/Wren/VirtualMachine.cs
+++ WrenSharp/Wren/VirtualMachine.cs
@@ -28,6 +28,123 @@ namespace Wren
             return WrenInterop.wrenInterpret(_handle, module, source);
         }
 
+        public int GetSlotCount()
+        {
+            return WrenInterop.wrenGetSlotCount(_handle);
+        }
+
+        public void EnsureSlots(int numSlots)
+        {
+            WrenInterop.wrenEnsureSlots(_handle, numSlots);
+        }
+
+        public ValueType GetSlotType(int slot)
+        {
+            return WrenInterop.wrenGetSlotType(_handle, slot);
+        }
+
+        public bool GetSlotBool(int slot)
+        {
+            return WrenInterop.wrenGetSlotBool(_handle, slot);
+        }
+
+        public string GetSlotBytes(int slot, out int length)
+        {
+            // TODO: Figure out how to use byte[] here...
+            throw new NotImplementedException();
+            // int outLength = 0;
+            // var bytes = WrenInterop.wrenGetSlotBytes(_handle, slot, ref outLength);
+            // length = outLength;
+            // return bytes;
+        }
+
+        public double GetSlotDouble(int slot)
+        {
+            return WrenInterop.wrenGetSlotDouble(_handle, slot);
+        }
+
+        public IntPtr GetSlotForeign(int slot)
+        {
+            throw new NotImplementedException();
+            // TODO: WrenInterop.wrenGetSlotForeign(_handle, int slot);
+        }
+
+        public string GetSlotString(int slot)
+        {
+            return WrenInterop.wrenGetSlotString(_handle, slot);
+        }
+
+        public Handle GetSlotHandle(int slot)
+        {
+            throw new NotImplementedException();
+            // TODO: return WrenInterop.wrenGetSlotHandle(_handle, int slot);
+        }
+
+        public void SetSlotBool(int slot, bool value)
+        {
+            WrenInterop.wrenSetSlotBool(_handle, slot, value);
+        }
+
+        public void SetSlotBytes(int slot, string bytes, uint length)
+        {
+            WrenInterop.wrenSetSlotBytes(_handle, slot, bytes, length);
+        }
+
+        public void SetSlotDouble(int slot, double value)
+        {
+            WrenInterop.wrenSetSlotDouble(_handle, slot, value);
+        }
+
+        public void SetSlotNewForeign(int slot, int classSlot, uint size)
+        {
+            throw new NotImplementedException();
+            // TODO: WrenInterop.wrenSetSlotNewForeign(_handle, int slot, int classSlot, uint size);
+        }
+
+        public void SetSlotNewList(int slot)
+        {
+            throw new NotImplementedException();
+            // TODO: WrenInterop.wrenSetSlotNewList(_handle, int slot);
+        }
+
+        public void SetSlotNull(int slot)
+        {
+            WrenInterop.wrenSetSlotNull(_handle, slot);
+        }
+
+        public void SetSlotString(int slot, string text)
+        {
+            WrenInterop.wrenSetSlotString(_handle, slot, text);
+        }
+
+        public void SetSlotHandle(int slot, Handle handle)
+        {
+            throw new NotImplementedException();
+            // TODO: WrenInterop.wrenSetSlotHandle(_handle, int slot, WrenHandle* handle);
+        }
+
+        public int GetListCount(int slot)
+        {
+            return WrenInterop.wrenGetListCount(_handle, slot);
+        }
+
+        public void GetListElement(int listSlot, int index, int elementSlot)
+        {
+            throw new NotImplementedException();
+            // TODO: WrenInterop.wrenGetListElement(_handle, int listSlot, int index, int elementSlot);
+        }
+
+        public void InsertInList(int listSlot, int index, int elementSlot)
+        {
+            throw new NotImplementedException();
+            // TODO: WrenInterop.wrenInsertInList(_handle, int listSlot, int index, int elementSlot);
+        }
+
+        public void AbortFiber(int slot)
+        {
+            WrenInterop.wrenAbortFiber(_handle, slot);
+        }
+
         private void WireEvents(ref Internal.Configuration config)
         {
             config.writeFn = (_, text) => OnWrite(text);
