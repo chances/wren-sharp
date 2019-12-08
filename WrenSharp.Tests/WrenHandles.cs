598079d3b05c4d77fc055cf37d2acb016db356b8
Chance Snow <git@chancesnow.me>
2019-12-07 16:49:38 -0800

    Add test to make and call a Wren call handle

--- WrenSharp.Tests/WrenHandles.cs
+++ WrenSharp.Tests/WrenHandles.cs
@@ -42,5 +42,36 @@ namespace WrenSharp.Tests
 
             vm.Dispose();
         }
+
+        [Fact]
+        public void WrenCallClassMethod()
+        {
+            const string module = "methodCall";
+            var source = @"class GameEngine { 
+  static update(elapsedTime) { 
+    // ... do something...
+  } 
+}";
+            var elapsedTime = 0.5d;
+
+            var vm = new VirtualMachine();
+            var result = vm.Interpret(module, source);
+            Assert.True(result == InterpretResult.WREN_RESULT_SUCCESS);
+
+            vm.EnsureSlots(2);
+            vm.GetVariable(module, "GameEngine", 0);
+            vm.SetSlotDouble(1, elapsedTime);
+
+            var updateMethod = vm.MakeCallHandle("update(_)");
+            Assert.True(updateMethod.HasReference, "Update method call handle has reference");
+            result = vm.Call(updateMethod);
+            Assert.True(result == InterpretResult.WREN_RESULT_SUCCESS);
+
+            // Verify return value
+            Assert.True(vm.GetSlotType(0) == ValueType.WREN_TYPE_NULL);
+
+            updateMethod.Dispose();
+            vm.Dispose();
+        }
     }
 }
